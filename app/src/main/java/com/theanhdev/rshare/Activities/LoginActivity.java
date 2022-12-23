340c875ce28fc570fc0cb23e47d5e1582d284953
TheAnhDev <akaidosuki@gmail.com>
2022-12-23 23:10:29 +0700

    update fragment

--- app/src/main/java/com/theanhdev/rshare/Activities/LoginActivity.java
+++ app/src/main/java/com/theanhdev/rshare/Activities/LoginActivity.java
@@ -1,67 +1,86 @@
 package com.theanhdev.rshare.Activities;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.theanhdev.rshare.MainActivity;
 import com.theanhdev.rshare.R;
 
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
     private ImageView logo;
-    private LinearLayout inputUser;
+    private LinearLayout inputUser, MadeBy;
     private TextView SignBtn, SignText;
-    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
+    private final FirebaseAuth mAuth = FirebaseAuth.getInstance();
     private EditText EmailInput, PasswordInput, ConfirmPasswordInput;
     private String CODE_SIGN = "login", email, password, confirm_password;
     public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
             Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+    private final FirebaseUser user = mAuth.getCurrentUser();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         binding();
-        mAuth = FirebaseAuth.getInstance();
         Handler handler = new Handler();
         handler.postDelayed(() -> logo.startAnimation(AnimationUtils.loadAnimation(LoginActivity.this, R.anim.zoom_out_up)), 2500);
-        Handler handler1 = new Handler();
-        handler1.postDelayed(() -> {
+        handler.postDelayed(() -> {
             inputUser.setVisibility(View.VISIBLE);
+            MadeBy.setVisibility(View.VISIBLE);
+            MadeBy.startAnimation(AnimationUtils.loadAnimation(LoginActivity.this, R.anim.fade_in_up));
             inputUser.startAnimation(AnimationUtils.loadAnimation(LoginActivity.this, R.anim.fade_in_up));
         }, 3900);
         SwitchSign();
+        if (user != null) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else SignActivity();
     }
     private void binding() {
         logo = findViewById(R.id.logo);
+        MadeBy = findViewById(R.id.madeBy);
         inputUser = findViewById(R.id.inputUser);
         SignBtn = findViewById(R.id.SignBtn);
         SignText = findViewById(R.id.signText);
         EmailInput = findViewById(R.id.email);
         PasswordInput = findViewById(R.id.password);
         ConfirmPasswordInput = findViewById(R.id.confirm_password);
+        progressBar = findViewById(R.id.PROGRESS);
     }
+    @SuppressLint("SetTextI18n")
     private void SwitchSign() {
         SignText.setOnClickListener(v -> {
             String text = SignText.getText().toString().trim();
             switch (text){
                 case "Create new account":
-                    SignText.setText("Haved an account");
+                    SignText.setText("Have an account");
                     SignBtn.setText("Sign Up");
                     ConfirmPasswordInput.setVisibility(View.VISIBLE);
                     CODE_SIGN = "signup";
                     break;
-                case "Haved an account":
+                case "Have an account":
                     SignText.setText("Create new account");
                     SignBtn.setText("Login");
                     ConfirmPasswordInput.setVisibility(View.GONE);
@@ -75,11 +94,41 @@ public class LoginActivity extends AppCompatActivity {
 
     private void SignActivity() {
         SignBtn.setOnClickListener(v -> {
+            progressBar.setVisibility(View.VISIBLE);
             switch (CODE_SIGN) {
                 case "login":
-                    mAuth.signInWithEmailAndPassword()
+                    if (formatText()) {
+                        mAuth.signInWithEmailAndPassword(email, password)
+                                .addOnCompleteListener(this, task -> {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Intent intent = new Intent(this, MainActivity.class);
+                                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(intent);
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        toast(Objects.requireNonNull("Người dùng không tồn tại"));
+                                    }
+                                    progressBar.setVisibility(View.GONE);
+                                });
+                    }
+                    break;
+                case "signup":
+                    if (formatText()) {
+                        mAuth.createUserWithEmailAndPassword(email, password)
+                                .addOnCompleteListener(this, task -> {
+                                    if (task.isSuccessful()) {
+                                        Intent intent = new Intent(this, MainActivity.class);
+                                        startActivity(intent);
+                                    } else {
+                                        toast(Objects.requireNonNull(task.getException().toString()));
+                                    }
+                                    progressBar.setVisibility(View.GONE);
+                                });
+                    }
                     break;
             }
+            progressBar.setVisibility(View.GONE);
         });
     }
 
@@ -87,17 +136,32 @@ public class LoginActivity extends AppCompatActivity {
         Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
     }
 
-    private void formatText() {
+    private boolean formatText() {
+        boolean checkEmail = false, checkPass = false, checkConfirmPass = false;
         email = EmailInput.getText().toString().trim();
-        if (!validateEmail(email)) EmailInput.setError("Invalid Email!");
-        else email = EmailInput.getText().toString().trim();
-        password = EmailInput.getText().toString().trim();
+        if (email.isEmpty()) EmailInput.setError("Email is empty");
+        else if (!validateEmail(email)) EmailInput.setError("Invalid Email!");
+        else checkEmail = true;
+        password = PasswordInput.getText().toString().trim();
+        if (validPassword(password).equals("Password is empty")) PasswordInput.setError(validPassword(password));
+        else if (validPassword(password).equals("Password less than six characters")) PasswordInput.setError(validPassword(password));
+        else checkPass = true;
         if (ConfirmPasswordInput.getVisibility() == View.VISIBLE) {
             confirm_password = ConfirmPasswordInput.getText().toString().trim();
-        }
+            if (!TextUtils.equals(confirm_password, password)) ConfirmPasswordInput.setError("Confirm password not match");
+            else checkConfirmPass = true;
+        } else checkConfirmPass = true;
+        return checkEmail && checkPass && checkConfirmPass;
     }
     public static boolean validateEmail(String emailStr) {
         Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailStr);
         return matcher.find();
+    }
+    public static String validPassword(String passwordStr) {
+        if (TextUtils.isEmpty(passwordStr)) {
+            return "Password is empty";
+        } else if (passwordStr.length() < 6) {
+            return "Password less than six characters";
+        } else return "TRUE";
     }
 }
--- app/src/main/java/com/theanhdev/rshare/MainActivity.java
+++ app/src/main/java/com/theanhdev/rshare/MainActivity.java
@@ -1,14 +1,52 @@
 package com.theanhdev.rshare;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.theanhdev.rshare.fragment.AccountFragment;
+import com.theanhdev.rshare.fragment.HomeFragment;
+import com.theanhdev.rshare.fragment.NotiFragment;
+import com.theanhdev.rshare.fragment.RvidFragment;
+import com.theanhdev.rshare.fragment.SearchFragment;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadFragment(new HomeFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.menu);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    loadFragment(new HomeFragment());
+                    break;
+                case R.id.search:
+                    loadFragment(new SearchFragment());
+                    break;
+                case R.id.rvideo:
+                    loadFragment(new RvidFragment());
+                    break;
+                case R.id.notification:
+                    loadFragment(new NotiFragment());
+                    break;
+                case R.id.Me:
+                    loadFragment(new AccountFragment());
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fl_wrapper, fragment);
+        transaction.addToBackStack("replacement");
+        transaction.setReorderingAllowed(true);
+        transaction.commit();
     }
 }
